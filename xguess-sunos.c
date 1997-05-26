8893b482f65dd4e47c35114aa6136911078654e5
David Arnold <d@0x1.org>
1997-05-26 10:56:12 +0000

    added dstc copyright

--- xguess-sunos.c
+++ xguess-sunos.c
@@ -1,3 +1,32 @@
+/***************************************************************
+ Copyright (C) DSTC Pty Ltd (ACN 052 372 577) 1997.
+ Unpublished work.  All Rights Reserved.
+ 
+ The software contained on this media is the property of the DSTC
+ Pty Ltd.  Use of this software is strictly in accordance with the
+ license agreement in the accompanying LICENSE.DOC file.  If your
+ distribution of this software does not contain a LICENSE.DOC file
+ then you have no rights to use this software in any manner and
+ should contact DSTC at the address below to determine an
+ appropriate licensing arrangement.
+ 
+      DSTC Pty Ltd
+      Level 7, Gehrmann Labs
+      University of Queensland
+      St Lucia, 4072
+      Australia
+      Tel: +61 7 3365 4310
+      Fax: +61 7 3365 4311
+      Email: enquiries@dstc.edu.au
+ 
+ This software is being provided "AS IS" without warranty of any
+ kind, and DSTC Pty Ltd disclaims all warranties.
+ 
+ Project:  Hector
+ File:     $Source$
+
+****************************************************************/
+
 /* xkeycaps, Copyright (c) 1991, 1992, 1993, 1994, 1996
  * by Jamie Zawinski <jwz@netscape.com>
  *
@@ -15,6 +44,11 @@
    this code in guess.c, but vuid_event.h defines a `struct keyboard' 
    that conflicts with our own...
  */
+
+#if !defined(lint)
+static const char rcsid[] = "@(#)$RCSfile$ $Revision$";
+#endif
+
 
 static char *xguess_sunos_dummy = "warning prevention";
 
